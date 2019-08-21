64761bb7d080de3302c3fdbeb2a2defb8bd22b8d
jfitzsimmons <jfitzhome@gmail.com>
2019-08-20 22:58:05 -0500

    images / style / basic functionality

--- js/clouds.js
+++ js/clouds.js
@@ -1,8 +1,12 @@
-const CLOUDS = (function() {
-  let stability = 33; // more about the condenseness of parts /  less drastic /  less weighted / goes down if higher than 75
-  let temp = -33; // more drastic / weighted
-  let moist = 33; // less drastic /  less weighted / goes down if higher than 75
-  const parts = [];
+let CLOUDS = (function() {
+  let stability = 8;
+  let temp = 5;
+  let moisture = 2;
+  console.log(`${stability} | ${temp} | ${stability}`);
+  //  let stability = 33; // more about the condenseness of parts /  less drastic /  less weighted / goes down if higher than 75
+  //  let temp = -33; // more drastic / weighted
+  //  let moist = 33; // less drastic /  less weighted / goes down if higher than 75
+  let parts = [];
   const cvs = {
     element: document.getElementById('clouds'),
     initialize: function() {
@@ -13,56 +17,29 @@ const CLOUDS = (function() {
       document.body.appendChild(this.element);
     },
   };
-  // let prevCloud =
-  let topY = document.getElementById('init-cloud').getBoundingClientRect().top;
-  let btmY =  document.getElementById('init-cloud').getBoundingClientRect().bottom;
-  let rightX = document.getElementById('init-cloud').getBoundingClientRect().right;
-  let leftX =  document.getElementById('init-cloud').getBoundingClientRect().left;
-  let widthX = document.getElementById('init-cloud').getBoundingClientRect().width;
-  let heightY =  document.getElementById('init-cloud').getBoundingClientRect().height;
-  let xmain = 45;
 
   let xlow = 45;
   let xhigh = 45;
-  let ylow = 0;
-  let yhigh = 0;
-console.log(`heightY:  ${heightY}`);
-console.log(`widthX:  ${widthX}`);
-  //console.log(`frame: ${frameY}`);
-  const Part = {
+  let ylow = 0 - temp;
+  let yhigh = 0 - temp;
+
+  let Part = {
     create: function() {
       const newPart = Object.create(this);
-      newPart.x = rndmRng((xlow - 7), (xhigh + 7));
 
+      newPart.x = rndmRng((xlow - stability), (xhigh + stability));
       if (newPart.x > xhigh) xhigh = newPart.x;
       if (newPart.x < xlow) xlow = newPart.x;
-      // newPart.x = rndmRng((-100), (100));
-      //(newPart.x >= 0) ? frameX += newPart.x: frameNegX += newPart.x;
 
-      // calculating where 1st circle goes vertically
-      //    newPart.y = rndmRng((cvs.height / 2), (cvs.height / 2) + temp - moist - stability );
-      //  newPart.y = rndmRng((cvs.height / 2), (cvs.height / 2));
-    //  newPart.y = rndmRng((heightY), (-heightY));
-      newPart.y = rndmRng((ylow - 3), (yhigh + 3));
+      newPart.y = rndmRng((ylow - 2.6 + (stability * .1)), (yhigh + 2.6 + (stability * .1)));
       if (newPart.y > yhigh) yhigh = newPart.y;
       if (newPart.y < ylow) ylow = newPart.y;
-//(newPart.y >= midY) ? frameY += (heightY - newPart.x) : frameNegX += newPart.x;
-
-
-  //console.log(`newPart.y CGK 1st CONDITIO- startY: ${startY} | newPart.y: ${newPart.y}`);
-
-    //  (newPart.y >= startY) ? frameY += newPart.y : frameNegY += newPart.y;
-      // (newPart.y >= startY) ? startY -= newPart.y : frameNegY += newPart.y;
-
-
 
       newPart.element = document.createElement('div');
 
-    //  newPart.element.style.top = `${newPart.y}px`;
-      //newPart.element.style.left = `${xPercent}%`;
       newPart.element.style.left = `${newPart.x}%`;
       newPart.element.style.marginTop = `${newPart.y}%`;
-      //newPart.element.style.left = `${newPart.x}px`;
+
       /*
             jQuery(newPart.element).css({
               width: `${newPart.radius}px`,
@@ -74,32 +51,28 @@ console.log(`widthX:  ${widthX}`);
             */
 
       newPart.element.classList.add('particle');
+      newPart.element.classList.add('remove');
       cvs.element.appendChild(newPart.element);
 
-
-
-
-
-      //console.log(`recheck width: ${frameX}`);
-
-      //  frame = newPart.element.getBoundingClientRect().left;
       return newPart;
     },
-    draw: function(c) {
-
-    },
   };
-  const partInit = (c) => requestAnimationFrame(() => c.draw(c));
   const rndmRng = (h, l) => Math.random() * (h - l) + l;
-  // const rndmArrI = (a) => a[Math.floor(Math.random() * a.length)];
 
   /**   */
   function newPart() {
     while (parts.length < 100) {
       parts.push(Part.create());
-      // count++;
     }
+    darken();
   }
+
+  function darken() {
+    elements = document.getElementsByClassName('particle');
+    for (var i = 0; i < elements.length; i++) {
+        elements[i].style.backgroundColor=`rgba(111,111,111,0.${moisture})`;
+    }
+}
   /*
     let timeout = false;
     window.addEventListener('resize', function() {
@@ -119,6 +92,25 @@ console.log(`widthX:  ${widthX}`);
     }
     */
 
+  const removeElements = (elms) => elms.forEach(el => el.remove());
+
+  document.getElementById('generate').addEventListener('click', function(e) {
+    e.preventDefault();
+    parts = [];
+    xlow = 45;
+    xhigh = 45;
+    ylow = 0 - temp;
+    yhigh = 0 - temp;
+    removeElements( document.querySelectorAll('.remove'));
+    stability = Math.abs(document.getElementById('stable').value - 15);
+    temp = Math.abs(document.getElementById('temp').value - 10);
+    moisture = document.getElementById('moisture').value;
+console.log(`${stability} | ${temp} | ${stability}`);
+    cvs.initialize();
+    newPart();
+  });
+
   cvs.initialize();
   newPart();
+
 })();
